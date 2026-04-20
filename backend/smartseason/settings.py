005d41ef7012634396e4a6378dcf535f7d59eed3
hesbon limo <limohesbon7@gmail.com>
2026-04-20 13:19:57 +0300

    added frontend url

--- backend/smartseason/settings.py
+++ backend/smartseason/settings.py
@@ -95,6 +95,7 @@ CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
     'http://localhost:5173',    
     'http://127.0.0.1:5173',
+    'https://smartseason-1-6595.onrender.com',
    
 ]
 CORS_ALLOW_CREDENTIALS = True
