d5cd3959527310784e697b7dc916586fef01ec70
hesbon limo <limohesbon7@gmail.com>
2026-04-20 14:03:25 +0300

    trying to solve the super user problem

--- backend/api/seed_users.py
+++ backend/api/seed_users.py
@@ -1,8 +1,11 @@
 from django.contrib.auth.models import User
+from .models import UserProfile
 
 def create_default_users():
+
+    # SUPERUSER (REAL ADMIN)
     if not User.objects.filter(username="admin").exists():
-        User.objects.create_superuser(
+        user = User.objects.create_superuser(
             username="admin",
             email="admin@gmail.com",
             password="admin123"
@@ -11,16 +14,16 @@ def create_default_users():
         user = User.objects.get(username="admin")
         user.is_staff = True
         user.is_superuser = True
+        user.set_password("admin123")
         user.save()
 
+    UserProfile.objects.get_or_create(user=user, defaults={"role": "admin"})
+
+    # NORMAL USERS
     if not User.objects.filter(username="user1").exists():
-        User.objects.create_user(
-            username="user1",
-            password="user123"
-        )
+        u1 = User.objects.create_user(username="user1", password="user123")
+        UserProfile.objects.create(user=u1, role="agent")
 
     if not User.objects.filter(username="user2").exists():
-        User.objects.create_user(
-            username="user2",
-            password="user456"
-        )
+        u2 = User.objects.create_user(username="user2", password="user456")
+        UserProfile.objects.create(user=u2, role="agent")
